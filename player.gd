fc2129e0d86eb243ecb08ce0a6a6eed499b8c755
Daniel Reed <mirata9@hotmail.com>
2026-05-16 19:30:26 +0800

    doctor

--- player.gd
+++ player.gd
@@ -8,13 +8,14 @@ enum State {
 }
 
 @export_category(("Stats"))
-@export var speed: int = 130
+@export var speed: int = 100
 
 var state: State = State.IDLE
 var move_direction: Vector2 = Vector2.ZERO
+var last_facing: Vector2 = Vector2.DOWN
 
 @onready var animation_tree: AnimationTree = $AnimationTree
-@onready var animation_playback: AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
+#@onready var animation_playback: AnimationNodeStateMachinePlayback = $AnimationTree["parameters/playback"]
 
 func _physics_process(delta: float) -> void:
 	movement_loop()
@@ -26,27 +27,28 @@ func movement_loop() -> void:
 	set_velocity(motion)
 	move_and_slide()
 	
-	if state == State.IDLE or state == State.RUN:
-		if move_direction.x < -0.01:
-			$WarriorBlue.flip_h = true
-		elif move_direction.x > 0.01:
-			$WarriorBlue.flip_h = false
+	if move_direction != Vector2.ZERO:
+		last_facing = Vector2(abs(move_direction.x), -move_direction.y).normalized()
+	animation_tree.set("parameters/blend_position", last_facing)
+	
+	if (state == State.IDLE or state == State.RUN) and velocity.x != 0:
+		$TomBaker.flip_h = velocity.x < 0
 	
 	
 	if motion != Vector2.ZERO and state == State.IDLE:
 		state = State.RUN
-		update_anmation()
+		#update_anmation()
 	elif motion == Vector2.ZERO and state == State.RUN:
 		state = State.IDLE
-		update_anmation()
+		#update_anmation()
 
 
-func update_anmation() -> void:
-	match state:
-		State.IDLE:
-			animation_playback.travel("idle")
-		State.RUN:
-			animation_playback.travel("run")
-		State.ATTACK:
-			animation_playback.travel("attack")
-				
+#func update_anmation() -> void:
+	#match state:
+		#State.IDLE:
+			#animation_playback.travel("idle")
+		#State.RUN:
+			#animation_playback.travel("run")
+		#State.ATTACK:
+			#animation_playback.travel("attack")
+				#
