547bc0215a9e214ab9abc0ec71f220418d916cb9
Daniel Reed <mirata9@hotmail.com>
2026-05-16 14:44:46 +0800

    restructure

--- player.gd
+++ player.gd
@@ -8,7 +8,7 @@ enum State {
 }
 
 @export_category(("Stats"))
-@export var speed: int = 300
+@export var speed: int = 130
 
 var state: State = State.IDLE
 var move_direction: Vector2 = Vector2.ZERO
